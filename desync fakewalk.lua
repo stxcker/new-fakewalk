b9cd2d464096dfcd896afc3704b1328ed7f59e69
stackergg <37311082+stackergg@users.noreply.github.com>
2020-04-22 15:25:28 -0400

    Fixed bug with incorrect flick times

--- desync fakewalk.lua
+++ desync fakewalk.lua
@@ -81,11 +81,56 @@ local function quick_stop( cmd )
 	cmd.sidemove = new_move.y
 end
 
+local function get_flick_tick( )
+	local weapon = entity.get_player_weapon( entity.get_local_player( ) )
+	
+	-- Modify the flick time of heavier weapons
+	if weapon == 85
+		or weapon == 103
+		or weapon == 106
+		or weapon == 108
+		or weapon == 113
+		or weapon == 115
+		or weapon == 120
+		or weapon == 122
+		or weapon == 124 
+		or weapon == 131
+		or weapon == 144 
+		or weapon == 153 
+		or weapon == 157
+		or weapon == 159 
+		or weapon == 161 
+		or weapon == 163
+		or weapon == 165 
+		or weapon == 167 then
+		return 6
+	end
+	
+	-- Modify the flick time of the even heavier weapons
+	if weapon == 110 
+		or weapon == 127 
+		or weapon == 134
+		or weapon == 151 
+		or weapon == 169 
+		or weapon == 177 then
+		return 7
+	end
+	
+	-- Modify the flick time of the heaviest weapons (wtf)
+	if weapon == 129 
+		or weapon == 171 then
+		return 9
+	end
+	
+	return 5
+end
+
 -- [x]======================================[ Callbacks ]======================================[x]
 local fakewalking = false
 local stored_onshot = false
 local stored_limit = 0
 local flicks = 0
+local adebug = false
 client.set_event_callback( "setup_command", function( cmd )	
 	if ui.get( variance ) > 0 or ui.get( slowmotion ) then
 		return
@@ -107,6 +152,7 @@ client.set_event_callback( "setup_command", function( cmd )
 	local real_angles = vec_3( entity.get_prop( entity.get_local_player( ), "m_angAbsRotation" ) )
 	local fake_side = ( normalize_as_yaw( real_angles.y - eye_angles.y ) > 0 ) and -1 or 1
 	
+	cmd.allow_send_packet = false
 	fakewalking = true
 	ui.set( onshot, false )
 	ui.set( limit, 14 )
@@ -116,8 +162,10 @@ client.set_event_callback( "setup_command", function( cmd )
 		end
 	end
 
-	if cmd.chokedcommands == ( ui.get( limit ) - 5 ) then
+	local flick_tick = get_flick_tick( )
+	if cmd.chokedcommands == ( ui.get( limit ) - flick_tick ) then
 		flicks = flicks + 1
+		adebug = true
 		if ui.get( fakewalk_mode ) == "Opposite" then
 			cmd.yaw = eye_angles.y + ( 60 * fake_side )
 		elseif ui.get( fakewalk_mode ) == "Extend" then
@@ -125,10 +173,18 @@ client.set_event_callback( "setup_command", function( cmd )
 		elseif ui.get( fakewalk_mode ) == "Jitter" then
 			cmd.yaw = eye_angles.y + ( 60 * ( flicks % 2 == 0 and -1 or 1 ) )
 		end
+	else
+		adebug = false
 	end
 end )
 
 
 client.set_event_callback( "paint", function( )
 	ui.set_visible( fakewalk_mode, not ui.get( slowmotion ) and true or false )
+	if adebug then
+		renderer.text( 85, 72, 255, 255, 255, 255, "b", 0, "flick" )
+	end
+	
+	local weapon = entity.get_player_weapon( entity.get_local_player( ) )
+	renderer.text( 85, 80, 255, 255, 255, 255, "b", 0, tostring( weapon ) )
 end )
